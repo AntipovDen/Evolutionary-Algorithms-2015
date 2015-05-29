6ffd2957705327e59e652d7f73f24c6e4d75bee4
AntipovDen <antipovden@yandex.ru>
2015-05-30 02:58:19 +0300

    lab4 works

--- lab4/lab4.py
+++ lab4/lab4.py
@@ -1,10 +1,13 @@
 __author__ = 'dantipov'
 
-from math import sin, cos, exp
+from math import sin, cos, exp, isnan, isfinite
 from random import random, randint
 from time import time
+from numpy import sum
 
 x = [0] * 9
+nan = float('nan')
+inf = float('inf')
 
 
 def plus(x1, x2):
@@ -20,28 +23,39 @@ def mul(x1, x2):
 
 
 def div(x1, x2):
-    if x2 == 0: return float('nan')
+    if x2 == nan or x2 == 0: return nan
     return x1 / x2
 
 
-def pow(x1, x2):
+def power(x1, x2):
+    if x1 < 0 and x2 // 1 != x2 or x1 == 0 and x2 < 0: return nan
     return x1 ** x2
 
 
-functions = [plus, minus, mul, div, abs, sin, cos, exp, pow]
-twoArgFunctions = [plus, minus, mul, div, pow]
-oneArgFunctions = [abs, sin, cos, exp]
+def myCos(x1):
+    if x1 in [inf, -inf]: return nan
+    return cos(x1)
+
+
+def mySin(x1):
+    if x1 in [inf, -inf]: return nan
+    return sin(x1)
+
+
+functions = [plus, minus, mul, div, abs, mySin, myCos, exp, power]
+twoArgFunctions = [plus, minus, mul, div, power]
+oneArgFunctions = [abs, mySin, myCos, exp]
 zeroArgFunctions = ['var', 'const']
 
-maxDepth = 15
+maxDepth = 7
 
 # constants
 crossingoverProb = 0.9
 mutationProb = 0.01
-populationSize = 50
-nextGenSize = 40
+populationSize = 250
+nextGenSize = 200
 iterations = 100
-#population format: [(fitness(p), p) for i in range(populationSize)]
+# population format: [(fitness(p), p) for i in range(populationSize)]
 
 
 class node:
@@ -50,6 +64,26 @@ class node:
         self.children = children
         self.parent = parent
 
+    def __cmp__(self, other):
+        return 0
+
+    def __eq__(self, other):
+        return True
+
+    def __ne__(self, other):
+        return False
+
+    def __lt__(self, other):
+        return False
+
+    def __gt__(self, other):
+        return False
+
+    def __le__(self, other):
+        return True
+
+    def __ge__(self, other):
+        return True
 
     def setChildrensParents(self):
         if self.function in oneArgFunctions:
@@ -62,20 +96,15 @@ class node:
         if self.function in oneArgFunctions:
             try:
                 return self.function(self.children.calc())
-            except TypeError or OverflowError:
-                print(self.function.__name__)
-                self.print()
-                print(self.children.calc())
-                exit(0)
+            except OverflowError:
+                return inf
+
         elif self.function in twoArgFunctions:
             try:
                 return self.function(self.children[0].calc(), self.children[1].calc())
-            except TypeError or OverflowError:
-                print(self.function.__name__)
-                self.print()
-                print(self.children[0].calc())
-                print(self.children[1].calc())
-                exit(0)
+            except OverflowError:
+                return inf
+
         elif self.function in zeroArgFunctions:
             if self.function == 'var':
                 return x[self.children]
@@ -113,8 +142,8 @@ class node:
         else:
             print(self.children, end=' ')
 
-    def copy(self, parent = 0):
-        n = node(self.function, self.children, self.parent)
+    def copy(self, parent=0):
+        n = node(self.function, self.children, parent)
         if self.function in oneArgFunctions:
             n.children = self.children.copy(n)
         elif self.function in twoArgFunctions:
@@ -122,6 +151,20 @@ class node:
             n.children[1] = self.children[1].copy(n)
         return n
 
+    def cut(self, depth=0):
+        if depth != maxDepth:
+            if self.function in oneArgFunctions:
+                self.children.cut(depth + 1)
+            if self.function in twoArgFunctions:
+                self.children[0].cut(depth + 1)
+                self.children[1].cut(depth + 1)
+        elif self.function in functions:
+            if random() < 0.1:  #const
+                self.function = 'const'
+                children = randint(1, 9)
+            else:  #var
+                self.function = 'var'
+                children = randint(0, 8)
 
 
 def genTree(depth):
@@ -139,90 +182,119 @@ def genTree(depth):
         else:
             f = functions[randint(0, 8)]
             if f in oneArgFunctions:
-                return node(f, genTree(depth + 1))
+                res = node(f, genTree(depth + 1))
+                res.children.parent = res
+                return res
             else:
-                return node(f, [genTree(depth + 1), genTree(depth + 1)])
+                res = node(f, [genTree(depth + 1), genTree(depth + 1)])
+                res.children[0].parent = res
+                res.children[1].parent = res
+                return res
 
 
 def crossingover(trees):
     t1, t2 = trees[0].copy(), trees[1].copy()
-    if (random() > crossingoverProb):
-        return t1, t2
+    # if (random() > crossingoverProb):
+    #     return t1, t2
+
     n1, n2 = t1.selectNode(), t2.selectNode()
+    # print()
+    # n1.print()
+    # print(n1.parent)
+    # n2.print()
+    # print(n2.parent)
+
     if n1.parent != 0:
-        p1 = n1.parent
-        if p1.function in oneArgFunctions:
-            p1.children = n2
-        elif p1.children[0] == n1:
-            p1.children[0] = n2
+        if n1.parent.function in oneArgFunctions:
+            n1.parent.children = n2
+        elif n1.parent.children[0] == n1:
+            n1.parent.children[0] = n2
         else:
-            p1.children[1] = n2
+            n1.parent.children[1] = n2
     else:
         t1 = n2
 
     if n2.parent != 0:
-        p2 = n2.parent
-        if p2.function in oneArgFunctions:
-            p2.children = n1
-        elif p2.children[0] == n2:
-            p2.children[0] = n1
+        if n2.parent.function in oneArgFunctions:
+            n2.parent.children = n1
+        elif n2.parent.children[0] == n2:
+            n2.parent.children[0] = n1
         else:
-            p2.children[1] = n1
+            n2.parent.children[1] = n1
     else:
         t2 = n1
+
+    n1.parent, n2.parent = n2.parent, n1.parent
+    t1.cut()
+    t2.cut()
 
     return t1, t2
 
 
 def mutation(tree):
-    if random() < mutationProb:
-        n = tree.selectNode()
-        if n.parent != 0:
-            p = n.parent
-            if p.function in oneArgFunctions:
-                p.children = genTree(3)
-            elif p.children[0] == 2:
-                p.children[0] = genTree(3)
-            else:
-                p.children[1] = genTree(3)
+    # if random() < mutationProb:
+    n = tree.selectNode()
+    if n.parent != 0:
+        p = n.parent
+        if p.function in oneArgFunctions:
+            p.children = genTree(3)
+            p.children.parent = p
+        elif p.children[0] == 2:
+            p.children[0] = genTree(3)
+            p.children[0].parent = p
+        else:
+            p.children[1] = genTree(3)
+            p.children[1].parent = p
 
 
 def targetFunction():
-    sum([(i + 1) * x[i] for i in range(9)])
+    return sum([(i + 1) * x[i] for i in range(9)])
+
+
+testingSet = 200
 
 
 def fitness(tree):
-    start_time = time()
+    #   start_time = time()
     global x
     res = 0
-    for x1 in range(-5, 6):
-        for x2 in range(-5, 6):
-            for x3 in range(-5, 6):
-                for x4 in range(-5, 6):
-                    for x5 in range(-5, 6):
-                        for x6 in range(-5, 6):
-                            for x7 in range(-5, 6):
-                                for x8 in range(-5, 6):
-                                    for x9 in range(-5, 6):
-                                        x = [x1, x2, x3, x4, x5, x6, x7, x8, x9]
-                                        res += (tree.calc() - targetFunction()) ** 2
+    # for x1 in range(-5, 6, 5):
+    #     print('x1 =', x1)
+    #     for x2 in range(-5, 6, 5):
+    #         for x3 in range(-5, 6, 5):
+    #             for x4 in range(-5, 6, 5):
+    #                 for x5 in range(-5, 6, 5):
+    #                     for x6 in range(-5, 6, 5):
+    #                         for x7 in range(-5, 6, 5):
+    #                             for x8 in range(-5, 6, 5):
+    #                                 for x9 in range(-5, 6, 5):
+    #                                     x = [x1, x2, x3, x4, x5, x6, x7, x8, x9]
+    #                                     res += (tree.calc() - targetFunction()) ** 2
+    for i in range(testingSet):
+        x = [random() * 10.24 - 5.12 for j in range(9)]
+        try:
+            res += (tree.calc() - targetFunction()) ** 2
+        except Exception:
+            # tree.print()
+            print('Exception')
 
-    print('fitness calculated in', time() - start_time, 'sec')
-    return res / 10 ** 9
-
-
-
+            #   print('fitness calculated in', time() - start_time, 'sec')
+    return res / testingSet
 
 
 def reduction(population):
-    distribution = [max(p[0], 0) for p in population]
-    for i in range(1, populationSize):
+    distribution = [1 / p[0] for p in population if not isnan(p[0]) and isfinite(p[0])]
+
+    for i in range(1, len(distribution)):
         distribution[i] += distribution[i - 1]
     result = random() * distribution[-1]
-    par1 = population[min([i for i in range(populationSize) if distribution[i] > result])]
+
+    par1 = population[min([i for i in range(len(distribution)) if distribution[i] > result])][1]
+
     result = random() * distribution[-1]
-    par2 = population[min([i for i in range(populationSize) if distribution[i] > result])]
+    par2 = population[min([i for i in range(len(distribution)) if distribution[i] > result])][1]
     return par1, par2
+
 
 def nextGen(population):
     next_gen = [0] * populationSize
@@ -238,12 +310,24 @@ def nextGen(population):
     return next_gen
 
 
-
 population = [genTree(0) for i in range(populationSize)]
 population = [(fitness(i), i) for i in population]
+
+# p1, p2 = reduction(population)
+# p1.print()
+# print()
+# mutation(p1)
+# p1.print()
+# exit(0)
 print('generated')
-for i in iterations:
+
+for i in range(iterations):
+    print('iteration', i)
+    if i % 10 == 0:
+        population.sort()
+        population[0][1].print()
     population = nextGen(population)
 
 population.sort()
-population[0][1].print()
+for p in population[:10]:
+    p[1].print()
